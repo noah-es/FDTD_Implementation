5d7aede1e1797a316b2188d8255d044990fffd14
Noah Masegosa Cáceres <107566346+noah-es@users.noreply.github.com>
2024-07-22 19:10:06 +0200

    Update FDTD-last.py

--- FDTD-last.py
+++ FDTD-last.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 22 08:43:35 2023
-
-@author: maseo
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
-    
+import imageio
+
 # Simulation parameters
 dominio = 200  # Domain size, 200 cells
-tiempo =100 # Time steps
+tiempo = 200  # Time steps
 epsilon_vacio = 1  # Vacuum permittivity
-mu_vacio = 1 # Vacuum permeability
-epsilon_material = 4  # Wall pemittivity
-sigma=0.04 # Wall conductivity
+mu_vacio = 1  # Vacuum permeability
+epsilon_material = 4  # Wall permittivity
+sigma = 0.04  # Wall conductivity
 
 # We will consider the Δt and Δx are both the unit, 
 # so they will not appear explicitly in the field expressions.
@@ -32,10 +26,13 @@ hy = np.zeros(dominio)
 # The frecuency also is different for us, due to the change in the value of c.
 
 x = 0
-pi=3.1415927
-f=40.4
+pi = 3.1415927
+f = 0.21
 # alpha is a constant dependent of material conductivity and permittivity (vacuum and material)
-alpha=(sigma)/(2*epsilon_material*epsilon_vacio)
+alpha = (sigma) / (2 * epsilon_material * epsilon_vacio)
+
+# Initialize list to store frames
+frames = []
 
 # I now perform the computations for both fields. 
 # For this purpose, several loops are nested, so that Ex and Hy are calculated 
@@ -50,123 +47,64 @@ for t in range(1, tiempo + 1):
     # The calculation changes a little bit, because I introduce alpha, that has 
     # all the information of the wall.
     
-    for i in range(0,dominio):
-        if 80<= i <=100:
-            ez[i]=ez[i]*((1-alpha)/(1+alpha))+(hy[i-1]-hy[i])/(np.sqrt(mu_vacio*epsilon_vacio)*epsilon_material*(1+alpha))
+    for i in range(0, dominio):
+        if 80 <= i <= 100:
+            ez[i] = ez[i] * ((1 - alpha) / (1 + alpha)) + (hy[i - 1] - hy[i]) / (np.sqrt(mu_vacio * epsilon_vacio) * epsilon_material * (1 + alpha))
         else:
-            ez[i]=ez[i]+(hy[i-1]-hy[i])/(np.sqrt(mu_vacio*epsilon_vacio))
+            ez[i] = ez[i] + (hy[i - 1] - hy[i]) / (np.sqrt(mu_vacio * epsilon_vacio))
            
-    pulso=np.sin(2*pi*f*t)
-    ez[x]=pulso
+    pulso = np.sin(2 * pi * f * t)
+    ez[x] = pulso
 
     # Calculation of Hy. 
     # Now, I CANNOT loop to 'dominio', due to it involves a 'ez[i+1]'
-    for i in range(0,dominio-1):
-        hy[i] = hy[i]+(ez[i]-ez[i+1])/np.sqrt(mu_vacio*epsilon_vacio)
-        
-# Here I use the format used in Sullivan. I mentioned it in de paper.
+    for i in range(0, dominio - 1):
+        hy[i] = hy[i] + (ez[i] - ez[i + 1]) / np.sqrt(mu_vacio * epsilon_vacio)
     
-plt.rcParams['font.size'] = 12
-plt.figure(figsize=(8, 3.5))
-plt.subplot(211)
-plt.plot(ez, color='k', linewidth=1)
-plt.ylabel('E$_z$', fontsize='14')
-plt.xticks(np.arange(0, 201, step=20))
-plt.xlim(-20, 200)
-plt.yticks(np.arange(-2, 3, step=1))
-plt.ylim(-2, 2)
-plt.text(80, 2.5, 'T = {}'.format(t),
-horizontalalignment='center')
-plt.text(115, 2.5, 'F = 40.4',
-horizontalalignment='center')
-plt.text(80, 2, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 1, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 0, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, -0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, -1, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, -1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -2, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 2, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 1, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 0, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -1, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -2, '|'.format(t),
-horizontalalignment='center')
+    # Create the plot for the current timestep
+    plt.rcParams['font.size'] = 12
+    plt.figure(figsize=(8, 3.5))
+    
+    plt.subplot(211)
+    plt.plot(ez, color='k', linewidth=1)
+    plt.ylabel('E$_z$', fontsize='14')
+    plt.xticks(np.arange(0, 201, step=20))
+    plt.xlim(-20, 200)
+    plt.yticks(np.arange(-2, 3, step=1))
+    plt.ylim(-2, 2)
+    plt.text(80, 2.5, 'T = {}'.format(t), horizontalalignment='center')
+    plt.text(115, 2.5, 'F = {}'.format(f), horizontalalignment='center')
+    for y in np.arange(2, -2.1, -0.5):
+        plt.text(80, y, '|', horizontalalignment='center')
+        plt.text(100, y, '|', horizontalalignment='center')
         
+    plt.subplot(212)
+    plt.plot(hy, color='k', linewidth=1)
+    plt.ylabel('H$_y$', fontsize='14')
+    plt.xlabel('Celdas FDTD')
+    plt.xticks(np.arange(0, 201, step=20))
+    plt.xlim(-20, 200)
+    plt.yticks(np.arange(-2, 3, step=1))
+    plt.ylim(-2, 2)
+    for y in np.arange(2, -2.1, -0.5):
+        plt.text(80, y, '|', horizontalalignment='center')
+        plt.text(100, y, '|', horizontalalignment='center')
+    
+    plt.subplots_adjust(bottom=0.2, hspace=0.45)
+    
+    # Save the current frame
+    plt.savefig(f'frame_{t}.png')
+    plt.close()
+    
+    frames.append(f'frame_{t}.png')
 
-plt.subplot(212)
-plt.plot(hy, color='k', linewidth=1)
-plt.ylabel('H$_y$', fontsize='14')
-plt.xlabel('Celdas FDTD')
-plt.xticks(np.arange(0, 201, step=20))
-plt.xticks(np.arange(0, 201, step=20))
-plt.xlim(-20, 200)
-plt.yticks(np.arange(-2, 3, step=1))
-plt.ylim(-2, 2)
-# plt.text(100, 2.2, 'T = {}'.format(t),
-# horizontalalignment='center')
-# plt.text(115, 2.5, 'F = 10GHz',
-# horizontalalignment='center')
-plt.text(80, 2, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 1, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, 0, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, -0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, -1, '|'.format(t),
-horizontalalignment='center')
-plt.text(80, -1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -2, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 2, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 1, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, 0, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -0.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -1, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -1.5, '|'.format(t),
-horizontalalignment='center')
-plt.text(100, -2, '|'.format(t),
-horizontalalignment='center')
+# Create GIF
+with imageio.get_writer('em_wave_propagation.gif', mode='I', duration=0.1) as writer:
+    for frame in frames:
+        image = imageio.imread(frame)
+        writer.append_data(image)
 
-plt.subplots_adjust(bottom=0.2, hspace=0.45)
-plt.show()
-
+# Clean up the frames
+import os
+for frame in frames:
+    os.remove(frame)
